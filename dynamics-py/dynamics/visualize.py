e4bcdd43995e3a30d2535a0428194b71ab30b7ce
Antoine Groudiev <antoine.groudiev@ens.psl.eu>
2025-04-12 18:08:04 -0400

    feat(viewer): display shapes

--- dynamics-py/dynamics/visualize.py
+++ dynamics-py/dynamics/visualize.py
@@ -1,13 +1,21 @@
 from . import dynamics
+import dynamics.collider as collider
 import meshcat
+import meshcat.geometry as mg
+import numpy as np
+from enum import Enum
+
+class GeometryType(Enum):
+    COLLISION = 1
+    VISUAL = 2
 
 class MeshcatVisualizer:
-    """A dynamics visualizer using meshcat."""
+    """A `dynamics` visualizer using Meshcat."""
     def __init__(
         self,
-        model=dynamics.Model(),
-        collision_model=None,
-        visual_model=None,
+        model: dynamics.Model | None = dynamics.Model(),
+        collision_model: dynamics.GeometryModel | None = None,
+        visual_model: dynamics.GeometryModel | None = None,
     ):
         # Check if the arguments are valid
         if collision_model is not None:
@@ -20,7 +28,12 @@ class MeshcatVisualizer:
         self.collision_model = collision_model
         self.visual_model = visual_model
 
-    def init_viewer(self, viewer=None, open=False, load_model=False, url=None):
+    def init_viewer(self,
+            viewer: meshcat.Visualizer | None = None,
+            open: bool = False,
+            load_model: bool = False,
+            url: str | None = None
+        ):
         """Start a new meschat server and client."""
 
         self.viewer = meshcat.Visualizer(url) if viewer is None else viewer
@@ -31,8 +44,62 @@ class MeshcatVisualizer:
         if load_model:
             self.load_model()
 
+    def load_shape(self, geometry: collider.PyShapeWrapper, geometry_type: GeometryType):
+        object = None
+
+        # Cylinders need to be rotated
+        R = np.array(
+            [
+                [1.0, 0.0, 0.0, 0.0],
+                [0.0, 0.0, -1.0, 0.0],
+                [0.0, 1.0, 0.0, 0.0],
+                [0.0, 0.0, 0.0, 1.0],
+            ]
+        )
+        RotatedCylinder = type(
+            "RotatedCylinder", (mg.Cylinder,), {"intrinsic_transform": lambda _self: R}
+        )
+        
+        if str(geometry.shape_type) == "ShapeType.Capsule": # quick fix until the enum comparison is fixed
+            object = RotatedCylinder(2.0 * geometry.half_length, geometry.radius)
+        elif str(geometry.shape_type) == "ShapeType.Cylinder":
+            object = RotatedCylinder(2.0 * geometry.half_length, geometry.radius)
+        elif str(geometry.shape_type) == "ShapeType.Cone":
+            object = RotatedCylinder(2.0 * geometry.half_length, 0, geometry.radius, 0)
+        # elif str(geometry.shape_type) == "ShapeType.Cuboid":
+        #     object = mg.Box(npToTuple(2.0 * geometry.half_side))
+        elif str(geometry.shape_type) == "ShapeType.Sphere":
+            object = mg.Sphere(geometry.radius)
+        else:
+            # TODO: proper error
+            assert False, "Geometry type not supported for visualization (type: {})".format(geometry.shape_type)
+
+        return object
+
+    def load_viewer_geometry_object(self, geometry_object: dynamics.GeometryObject, geometry_type: GeometryType):
+        geometry = geometry_object.geometry
+        meshcat_node = self.viewer[geometry_object.name]
+
+        if isinstance(geometry, collider.PyShapeWrapper):
+            object = self.load_shape(geometry, geometry_type)
+            meshcat_node.set_object(object)
+        else:
+            # TODO: proper error
+            assert False, "Geometry object is not a standard shape, cannot load it into viewer (type: {})".format(type(geometry))
+
     def load_model(self):
-        print("Warning: load_model is not implemented yet.")
+        if self.visual_model is not None:
+            for visual in self.visual_model.geometry_objects:
+                self.load_viewer_geometry_object(visual, GeometryType.VISUAL)
 
     def reset(self):
         self.viewer.delete()
+
+    def set_cam_target(self, target: np.ndarray):
+        self.viewer.set_cam_target(target)
+
+    def set_cam_pos(self, position: np.ndarray):
+        self.viewer.set_cam_pos(position)
+
+    def open(self):
+        self.viewer.open()
--- examples/python/display-shapes-meshcat.py
+++ examples/python/display-shapes-meshcat.py
@@ -5,7 +5,7 @@ import numpy as np
 geometries = [
     collider.Capsule(0.1, 0.8),
     collider.Sphere(0.5),
-    collider.Cuboid((1, 1, 1)),
+    # collider.Cuboid((1, 1, 1)),
     collider.Cylinder(0.1, 1.0),
     collider.Cone(0.5, 1.0),
 ]
@@ -16,8 +16,13 @@ geom_model = dynamics.GeometryModel()
 for i, geom in enumerate(geometries):
     placement = dynamics.SE3(np.eye(3), np.array([i, 0.0, 0.0]))
     geom_obj = dynamics.GeometryObject(f"obj{i}", 0, 0, geom, placement)
+    color = np.random.uniform(0, 1, 4)
+    color[3] = 1
+    geom_obj.mesh_color = color
+    geom_model.add_geometry_object(geom_obj)
 
-# viz = dynamics.visualize.MeshcatVisualizer(model, geom_model, geom_model)
-# viz.init_viewer(open=True, load_model=True)
+viz = dynamics.visualize.MeshcatVisualizer(model, geom_model, geom_model)
+viz.init_viewer(load_model=True)
 
-# input("Press enter to continue...")
+viz.open()
+input("Press enter to continue...")
