58713e260a38e30f1b692238253e432ae99da745
Antoine Groudiev <antoine.groudiev@ens.psl.eu>
2025-09-29 14:54:29 +0200

    feat(examples): add ur5 meshes

--- examples/python/inverse-dynamics.py
+++ examples/python/inverse-dynamics.py
@@ -2,7 +2,7 @@ import numpy as np
 import dynamics as dyn
 
 # load an URDF file
-model, geom_model = dyn.build_models_from_urdf("examples/descriptions/ur5_robot.urdf")
+model, geom_model = dyn.build_models_from_urdf("examples/descriptions/ur5/ur5_robot.urdf")
 
 # Build a data frame associated with the model
 data = model.create_data()
@@ -11,6 +11,12 @@ data = model.create_data()
 q = dyn.random_configuration(model)
 v = np.random.rand(model.nv, 1)
 a = np.random.rand(model.nv, 1)
+
+viz = dyn.visualize.MeshcatVisualizer(model, geom_model, geom_model)
+viz.init_viewer(load_model=True)
+
+viz.open()
+input("Press enter to continue...")
 
 # computes the inverse dynamics using Recursive Newton-Euler Algorithm (RNEA)
 # tau = dyn.rnea(model, data, q, v, a)
