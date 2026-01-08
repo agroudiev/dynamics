5289bd55f413883806e1c09b13332dfd1a83cd64
Antoine Groudiev <antoine.groudiev@ens.psl.eu>
2026-01-08 17:51:07 +0100

    test(urdf): start to fix materials test

--- crates/parse/src/tests.rs
+++ crates/parse/src/tests.rs
@@ -57,36 +57,40 @@ fn test_materials() {
     );
     assert_eq!(*geom_data.get_object_placement(0).unwrap(), SE3::identity());
 
+    dbg!(&model.joint_names);
+    dbg!(&model.joint_parents);
+
     // right leg
-    let right_leg_id = model.joint_index_by_name("base_to_right_leg").unwrap();
-    // TODO: retrieve geometry object by name
+    let base_right_leg_id = model.joint_index_by_name("base_to_right_leg").unwrap();
+    let right_leg_id = model.joint_index_by_name("right_leg").unwrap();
     assert_eq!(
         model
             .joint_placements
-            .get(&right_leg_id)
+            .get(&base_right_leg_id)
             .unwrap()
             .translation(),
         Vector3D::new(0.0, -0.22, 0.25)
     );
     // assert_eq!(geom_model.models.get(&right_leg_id).unwrap().parent_joint, right_leg_id);
     assert_eq!(
-        geom_data.get_object_placement(1).unwrap().translation(),
+        geom_data.get_object_placement(right_leg_id).unwrap().translation(),
         Vector3D::new(0.0, -0.22, 0.25 - 0.3)
     );
 
     // left leg
-    let left_leg_id = model.joint_index_by_name("base_to_left_leg").unwrap();
+    let base_left_leg_id = model.joint_index_by_name("base_to_left_leg").unwrap();
+    let left_leg_id = model.joint_index_by_name("left_leg").unwrap();
     assert_eq!(
         model
             .joint_placements
-            .get(&left_leg_id)
+            .get(&base_left_leg_id)
             .unwrap()
             .translation(),
         Vector3D::new(0.0, 0.22, 0.25)
     );
     // assert_eq!(geom_model.models.get(&left_leg_id).unwrap().parent_joint, left_leg_id);
     assert_eq!(
-        geom_data.get_object_placement(2).unwrap().translation(),
+        geom_data.get_object_placement(left_leg_id).unwrap().translation(),
         Vector3D::new(0.0, 0.22, 0.25 - 0.3)
     );
 }
