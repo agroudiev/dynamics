a92520d9182c8c32c24c5d4cdadf7fff7028a9f9
Antoine Groudiev <antoine.groudiev@ens.psl.eu>
2026-01-24 18:32:38 +0100

    chore: create entry-point crate

---
+++ crates/dynamics-rs/src/lib.rs
@@ -0,0 +1,8 @@
+//! # dynamics: A Rust library for Rigid Body Dynamics
+
+pub use collider_rs as collider;
+pub use dynamics_inertia as inertia;
+pub use dynamics_joint as joint;
+pub use dynamics_model as model;
+pub use dynamics_parse as parse;
+pub use dynamics_spatial as spatial;
