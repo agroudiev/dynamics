0fe269523e7be44038ddd5199900b4dabf1153b1
Antoine Groudiev <antoine.groudiev@ens.psl.eu>
2026-01-27 18:01:11 +0100

    feat(FK): add `v` and `a` parameters

--- crates/dynamics-model/src/forward_kinematics.rs
+++ crates/dynamics-model/src/forward_kinematics.rs
@@ -26,13 +26,33 @@ pub fn forward_kinematics(
     model: &Model,
     data: &mut Data,
     q: &Configuration,
+    v: &Option<Configuration>,
+    a: &Option<Configuration>,
 ) -> Result<(), ConfigurationError> {
-    // check if q is of the right size
+    // check if configurations are of the right size
     if q.len() != model.nq {
         return Err(ConfigurationError::InvalidParameterSize(
             "q".to_string(),
             model.nq,
             q.len(),
+        ));
+    }
+    if let Some(v) = v
+        && v.len() != model.nv
+    {
+        return Err(ConfigurationError::InvalidParameterSize(
+            "v".to_string(),
+            model.nv,
+            v.len(),
+        ));
+    }
+    if let Some(a) = a
+        && a.len() != model.nv
+    {
+        return Err(ConfigurationError::InvalidParameterSize(
+            "a".to_string(),
+            model.nv,
+            a.len(),
         ));
     }
 
@@ -72,11 +92,13 @@ pub fn forward_kinematics(
     Ok(())
 }
 
-#[pyfunction(name = "forward_kinematics")]
+#[pyfunction(name = "forward_kinematics", signature=(model, data, q, v=None, a=None))]
 pub fn py_forward_kinematics(
     model: &PyModel,
     data: &mut PyData,
     q: PyReadonlyArray1<f64>,
+    v: Option<PyReadonlyArray1<f64>>,
+    a: Option<PyReadonlyArray1<f64>>,
 ) -> PyResult<()> {
     let q = q.as_array();
     if q.shape() != [model.inner.nq] {
@@ -92,7 +114,45 @@ pub fn py_forward_kinematics(
     };
     let q = Configuration::from_row_slice(q);
 
-    forward_kinematics(&model.inner, &mut data.inner, &q)
+    let v = match v {
+        Some(v_array) => {
+            let v_array = v_array.as_array();
+            if v_array.shape() != [model.inner.nv] {
+                return Err(PyValueError::new_err(format!(
+                    "Invalid input size. Expected a configuration of size {}, got {:?}",
+                    model.inner.nv,
+                    v_array.shape()
+                )));
+            }
+            let v_slice = match v_array.as_slice() {
+                Some(slice) => slice,
+                None => return Err(PyValueError::new_err("Failed to convert v to slice")),
+            };
+            Some(Configuration::from_row_slice(v_slice))
+        }
+        None => None,
+    };
+
+    let a = match a {
+        Some(a_array) => {
+            let a_array = a_array.as_array();
+            if a_array.shape() != [model.inner.nv] {
+                return Err(PyValueError::new_err(format!(
+                    "Invalid input size. Expected a configuration of size {}, got {:?}",
+                    model.inner.nv,
+                    a_array.shape()
+                )));
+            }
+            let a_slice = match a_array.as_slice() {
+                Some(slice) => slice,
+                None => return Err(PyValueError::new_err("Failed to convert a to slice")),
+            };
+            Some(Configuration::from_row_slice(a_slice))
+        }
+        None => None,
+    };
+
+    forward_kinematics(&model.inner, &mut data.inner, &q, &v, &a)
         .map_err(|e| PyValueError::new_err(format!("Forward kinematics failed: {e:?}")))?;
     Ok(())
 }
