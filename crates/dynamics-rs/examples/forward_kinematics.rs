13eb52158480706bb4e697a7d49036fd9ce3f028
Antoine Groudiev <antoine.groudiev@ens.psl.eu>
2026-02-02 17:54:40 +0100

    doc: add examples

---
+++ crates/dynamics-rs/examples/forward_kinematics.rs
@@ -0,0 +1,38 @@
+use dynamics_model::forward_kinematics::update_frame_placements;
+use dynamics_model::model::random_configuration;
+use dynamics_rs::model::forward_kinematics::forward_kinematics;
+use dynamics_rs::parse::urdf::build_models_from_urdf;
+
+fn main() {
+    let urdf_path = "./examples/descriptions/ur5/ur5_robot.urdf"; // Path to the URDF file
+    let mesh_path = "./examples/descriptions/ur5/"; // Optional mesh path
+
+    // Build models from the URDF file
+    let (model, _coll_model, _viz_model) =
+        build_models_from_urdf(urdf_path, Some(mesh_path)).expect("Failed to parse URDF file");
+
+    // Generate a random configuration
+    let q = random_configuration(&model);
+    println!("Random configuration q: {:?}", q);
+
+    // Create data structure for the model
+    let mut data = model.create_data();
+
+    // Compute forward kinematics
+    forward_kinematics(&model, &mut data, &q, &None, &None)
+        .expect("Failed to compute forward kinematics");
+
+    // Print the placement of the joint 'wrist_3_joint'
+    let id = model.get_joint_id("wrist_3_joint").unwrap();
+    let placement = &data.joint_placements[id];
+    println!("Placement of 'wrist_3_joint':\n{:?}", placement);
+
+    // Compute the frame placements
+    update_frame_placements(&model, &mut data);
+
+    // Print the placement of the frame 'tool0'
+    let frame_id = model.get_frame_id("tool0", None).unwrap();
+    // we don't specify a frame type (None) as there is only one frame with this name
+    let frame_placement = &data.frame_placements[frame_id];
+    println!("Placement of frame 'tool0':\n{:?}", frame_placement);
+}
---
+++ crates/dynamics-rs/examples/parse_urdf.rs
@@ -0,0 +1,22 @@
+use dynamics_rs::parse::urdf::build_models_from_urdf;
+
+fn main() {
+    let urdf_path = "./examples/descriptions/ur5/ur5_robot.urdf"; // Path to the URDF file
+    let mesh_path = "./examples/descriptions/ur5/"; // Optional mesh path
+
+    // Build models from the URDF file
+    let (model, _coll_model, _viz_model) =
+        build_models_from_urdf(urdf_path, Some(mesh_path)).expect("Failed to parse URDF file");
+
+    // Print the names of the joints
+    println!("Joints in the model:");
+    for joint_name in &model.joint_names {
+        println!("- {}", joint_name);
+    }
+
+    // Print the names of the frames
+    println!("\nFrames in the model:");
+    for frame in &model.frames {
+        println!("- {}", frame.name);
+    }
+}
