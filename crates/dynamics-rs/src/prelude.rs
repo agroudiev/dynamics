38a37ff84a75be82015045e324eef0ffe7c821e0
Antoine Groudiev <antoine.groudiev@ens.psl.eu>
2026-02-18 22:19:55 +0100

    feat(prelude): add FD, spatial elements

--- crates/dynamics-rs/src/prelude.rs
+++ crates/dynamics-rs/src/prelude.rs
@@ -6,6 +6,8 @@ pub use crate::model::model::Model;
 pub use crate::parse::urdf::build_models_from_urdf;
 
 // Algorithms
+pub use crate::model::forward_dynamics::ABAConvention;
+pub use crate::model::forward_dynamics::forward_dynamics;
 pub use crate::model::forward_kinematics::forward_kinematics;
 pub use crate::model::forward_kinematics::update_frame_placements;
 pub use crate::model::inverse_dynamics::inverse_dynamics;
@@ -13,3 +15,8 @@ pub use crate::model::inverse_dynamics::inverse_dynamics;
 // Configurations
 pub use crate::model::model::random_configuration;
 pub use crate::spatial::configuration::Configuration;
+
+// Spatial
+pub use crate::spatial::force::SpatialForce;
+pub use crate::spatial::motion::SpatialMotion;
+pub use crate::spatial::se3::SE3;
